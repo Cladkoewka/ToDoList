cda26fa92ed78bbb12cd218ff112ffd03f745ad3
Cladkoewka <codewarriorms@gmail.com>
2024-09-22 17:38:50 +0300

    Bruh tests doesn't work

--- ToDoList.Domain/obj/Debug/net8.0/ToDoList.Domain.AssemblyInfo.cs
+++ ToDoList.Domain/obj/Debug/net8.0/ToDoList.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ToDoList.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61960fe534bd34c523fe373df66e414ffcb915e2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8350c246c91e3f223af65367804286fe964b0a0e")]
 [assembly: System.Reflection.AssemblyProductAttribute("ToDoList.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ToDoList.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
